f05b695e41d142655ce31116a532e25835e78abd
Riya052 <24057060@kiit.ac.in>
2026-04-27 11:53:55 +0530

    fix forgot password url

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -14,7 +14,8 @@ export default function ForgotPassword() {
     setLoading(true);
     setError('');
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/forgot-password', { email });
+      const res = await axios.post(
+      'https://job-tracker-1-5afr.onrender.com/api/auth/forgot-password', { email });
       setMessage(res.data.message);
       setSent(true);
     } catch (err) {
